a370c859353d9a183f4a157bbde44b6f08756b95
Aditya <41179329+1337w0rm@users.noreply.github.com>
2020-11-02 21:01:57 +0530

    Changed USERNAME to USER_NAME
    USERNAME environment variable was in conflict  with the system set USERNAME environment variable.

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 class Config(object):
 	BOT_TOKEN = os.environ.get('BOT_TOKEN')
-	USERNAME = os.environ.get('USERNAME')
+	USERNAME = os.environ.get('USER_NAME')
 	PASSWORD = os.environ.get('PASSWORD')
