a1574597388286c20ef949f93066fb0f564eb0bb
wrecker3000 <73797885+wrecker3000@users.noreply.github.com>
2020-11-03 02:15:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 class Config(object):
-	BOT_TOKEN = os.environ.get('BOT_TOKEN')
-	USERNAME = os.environ.get('USER_NAME')
-	PASSWORD = os.environ.get('PASSWORD')
+	BOT_TOKEN = os.environ.get('BOT_TOKEN', ' 1234567890:Paste_BotTokenHereI')
+	USERNAME = os.environ.get('USER_NAME', 'your@email.com')
+	PASSWORD = os.environ.get('PASSWORD', 'password')
