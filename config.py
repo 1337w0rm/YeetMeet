e86f5902c65fabc75e803948e05822449e645731
Aditya Singh <baddrink.87@gmail.com>
2020-11-08 13:08:38 +0530

    Added configuration information in comment

--- config.py
+++ config.py
@@ -1,5 +1,14 @@
 import os
 class Config(object):
 	BOT_TOKEN = os.environ.get('BOT_TOKEN')
-	USERNAME = os.environ.get('USERNAME')
-	PASSWORD = os.environ.get('PASSWORD')
+	GUSERNAME = os.environ.get('GUSER_NAME')
+	GPASSWORD = os.environ.get('GPASSWORD')
+
+# If you're not familiar with how to set Environment Variables you can do like this instead
+# of  setting Environment Variables
+
+# BOT_TOKEN = os.environ.get('BOT_TOKEN', 'BOT_TOKEN_HERE')
+# GUSERNAME = os.environ.get('GUSER_NAME', 'Google Email')
+# GPASSWORD = os.environ.get('GPASSWORD', 'Google Email Password')
+
+#NOTE: Putting sensitive information in files is considered unsafe. Try to use ENVIRONMENT VARIABLES 
