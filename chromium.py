dc11c36582cc0109eb6335fa58437ce2c354baa5
Aditya Singh <baddrink.87@gmail.com>
2020-11-08 13:14:31 +0530

    Added Zoom Leave Meeting Command

--- chromium.py
+++ chromium.py
@@ -30,12 +30,21 @@ def meetexit(update,context):
 	browser.quit()
 	execl(executable, executable, "chromium.py")
 
+@run_async
+def zoomexit(update,context):
+	context.bot.send_message(chat_id=451311925, text="Exiting Zoom Meeting")
+	browser.find_element_by_xpath('//*[@id="wc-container-left"]/div[4]/div/div/div/div[1]').click()
+	browser.find_element_by_xpath('//*[@id="wc-footer"]/div/div[3]/div/button').click()
+	browser.quit()
+	execl(executable, executable, "chromium.py")
+
 def main():
 	dp.add_handler(CommandHandler("zoom", zoom))
 	dp.add_handler(CommandHandler("meet", meet))
 	dp.add_handler(CommandHandler("restart", restart))
 	dp.add_handler(CommandHandler("status", status))
 	dp.add_handler(CommandHandler("meetexit", meetexit))
+	dp.add_handler(CommandHandler("zoomexit", zoomexit))
 	logging.info("Bot started")
 
 	updater.start_polling()
