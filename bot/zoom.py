fd4e9d015a333bd5cb33f1ac4752b447feb23c3c
Varenyam Bhardwaj <varenyambhardwaj123@gmail.com>
2021-08-06 12:17:26 +0530

    Temperory Fix to cookie error

--- bot/zoom.py
+++ bot/zoom.py
@@ -28,6 +28,7 @@ def joinZoom(context, url_meet, passStr):
             browser.quit()
             execl(executable, executable, "chromium.py")
     try:
+        """
         if os.path.exists("zoom.pkl"):
             cookies = pickle.load(open("zoom.pkl", "rb"))
             browser.get('https://accounts.google.com/o/oauth2/auth/identifier?client_id=717762328687-iludtf96g1hinl76e4lc1b9a82g457nn.apps.googleusercontent.com&scope=profile%20email&redirect_uri=https%3A%2F%2Fstackauth.com%2Fauth%2Foauth2%2Fgoogle&state=%7B%22sid%22%3A1%2C%22st%22%3A%2259%3A3%3Abbc%2C16%3Afad07e7074c3d678%2C10%3A1601127482%2C16%3A9619c3b16b4c5287%2Ca234368b2cab7ca310430ff80f5dd20b5a6a99a5b85681ce91ca34820cea05c6%22%2C%22cdl%22%3Anull%2C%22cid%22%3A%22717762328687-iludtf96g1hinl76e4lc1b9a82g457nn.apps.googleusercontent.com%22%2C%22k%22%3A%22Google%22%2C%22ses%22%3A%22d18871cbc2a3450c8c4114690c129bde%22%7D&response_type=code&flowName=GeneralOAuthFlow')
@@ -36,14 +37,14 @@ def joinZoom(context, url_meet, passStr):
         else:
             context.bot.send_message(chat_id=userId, text="You're not logged in! Send /zlogin to login to zoom")
             return
-
+        """
         browser.get('https://zoom.us/wc/join/'+ url_meet)
-        time.sleep(5)
+        time.sleep(10)
         browser.save_screenshot("ss.png")
         context.bot.send_chat_action(chat_id=userId, action=ChatAction.UPLOAD_PHOTO)
         mid  = context.bot.send_photo(chat_id=userId, photo=open('ss.png', 'rb'), timeout = 120).message_id
         os.remove('ss.png')
-
+        time.sleep(10)
         browser.find_element_by_xpath('//*[@id="joinBtn"]').click()
         time.sleep(5)
         try: 
@@ -69,7 +70,7 @@ def joinZoom(context, url_meet, passStr):
         WebDriverWait(browser, 1000).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="wc-container-right"]/div/div[2]/div/button[2]'))).click()
 
         context.bot.send_chat_action(chat_id=userId, action=ChatAction.TYPING)
-        context.bot.send_message(chat_id=userId, text="Attending you lecture. You can chill :v")
+        context.bot.send_message(chat_id=userId, text="Attending you to lecture. You can chill :v")
         logging.info("STAAAAPH!!")
 
     
